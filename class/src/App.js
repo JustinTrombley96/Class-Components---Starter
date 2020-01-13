b1451e6618003c88d4f2e36b1047facd356fa804
Justin Trombley <jtrombley96@gmail.com>
2020-01-13 12:10:39 -0500

    Starter Code

---
+++ class/src/App.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import GroceryList from './components/GroceryList';
+import ListForm from './components/ListForm';
+import './styles.css';
+
+const groceries = [
+  {
+    name: 'Bananas',
+    id: 123,
+    purchased: false
+  },
+  {
+    name: 'Torillas',
+    id: 124,
+    purchased: false
+  },
+  {
+    name: 'Milk',
+    id: 1235,
+    purchased: false
+  },
+  {
+    name: 'Pizza Sauce',
+    id: 1246,
+    purchased: false
+  },
+  {
+    name: 'Raw Honey',
+    id: 1237,
+    purchased: false
+  },
+  {
+    name: 'Granola',
+    id: 1248,
+    purchased: false
+  }
+];
+
+class App extends React.Component {
+  // Constructor with state
+
+  // Class methods to update state
+
+  render() {
+    console.log('rendering...');
+    return (
+      <div className="App">
+        <div className="header">
+          <h1>Shopping List</h1>
+          <ListForm />
+        </div>
+        <GroceryList groceries={groceries} />
+      </div>
+    );
+  }
+}
+
+
