57e2ff0751e08e6614cd01d65c9d3e695ff71816
Justin Trombley <jtrombley96@gmail.com>
2020-01-13 12:28:28 -0500

    Super, Constructor, State

--- class/src/App.js
+++ class/src/App.js
@@ -1,59 +1,80 @@
-import React, {Component, useState}from 'react';
+import React, { Component, useState } from 'react';
 
 import GroceryList from './components/GroceryList';
 import ListForm from './components/ListForm';
 import './styles.css';
 
 const groceries = [
-  {
-    name: 'Bananas',
-    id: 123,
-    purchased: false
-  },
-  {
-    name: 'Torillas',
-    id: 124,
-    purchased: false
-  },
-  {
-    name: 'Milk',
-    id: 1235,
-    purchased: false
-  },
-  {
-    name: 'Pizza Sauce',
-    id: 1246,
-    purchased: false
-  },
-  {
-    name: 'Raw Honey',
-    id: 1237,
-    purchased: false
-  },
-  {
-    name: 'Granola',
-    id: 1248,
-    purchased: false
-  }
+	{
+		name      : 'Bananas',
+		id        : 123,
+		purchased : false,
+	},
+	{
+		name      : 'Torillas',
+		id        : 124,
+		purchased : false,
+	},
+	{
+		name      : 'Milk',
+		id        : 1235,
+		purchased : false,
+	},
+	{
+		name      : 'Pizza Sauce',
+		id        : 1246,
+		purchased : false,
+	},
+	{
+		name      : 'Raw Honey',
+		id        : 1237,
+		purchased : false,
+	},
+	{
+		name      : 'Granola',
+		id        : 1248,
+		purchased : false,
+	},
 ];
 
 class App extends Component {
-  // Constructor with state
-
-  // Class methods to update state
-
-  render() {
-    console.log('rendering...');
-    return (
-      <div className="App">
-        <div className="header">
-          <h1>Shopping List</h1>
-          <ListForm />
-        </div>
-        <GroceryList groceries={groceries} />
-      </div>
-    );
+	// Constructor with state
+	constructor () {
+    super();
+    // initialize the state object
+    this.state = {
+      groceryList: groceries
+    }
   }
+
+
+//----------------------------------------------------
+
+  //Function Components with State
+  // const [name, setName] = useState('Dustin')
+  // const [hogwartsHouse, setHogwartsHouse] = useState('Gryffindor')
+
+  // setName('Dusty')
+  // this.setState({name: 'Dusty'})
+  
+//----------------------------------------------------
+	// Class methods to update state
+
+	//render - comparable to a function component's return
+	// render is in charge of ... rendering JSX!
+
+	render () {
+		console.log('rendering...');
+		return (
+			<div className='App'>
+				<div className='header'>
+					<h1>Shopping List</h1>
+					<ListForm />
+				</div>
+				<GroceryList groceries={this.state.groceryList} />
+			</div>
+		);
+	}
 }
 
-export default App
+export default App;
